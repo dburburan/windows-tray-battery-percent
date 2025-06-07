61fc089876bf041cb3fc9147a80f2a2f89238885
David <dburburan@gmail.com>
2025-06-07 14:24:11 +1000

    Fix fail-to-update bug

--- src/battery_monitor.rs
+++ src/battery_monitor.rs
@@ -1,6 +1,12 @@
 use battery::Manager;
 use crate::debug_util::dmsg;
 
+#[derive(Clone, PartialEq, Eq)]
+pub struct BatteryInfo {
+	pub percentage: i32,
+	pub is_charging: bool,
+}
+
 pub struct BatteryMonitor {
 	manager: Manager,
 }
@@ -18,7 +24,7 @@ impl BatteryMonitor {
 		})
 	}
 
-	pub fn get_battery_info(&self) -> Result<(i32, bool), String> {
+	pub fn get_battery_info(&self) -> Result<BatteryInfo, String> {
 		match self.manager.batteries() {
 			Err(e) => { Err(format!("Failed to retrieve batteries: {:?}", e)) }
 			Ok(mut batteries) => {
@@ -30,7 +36,7 @@ impl BatteryMonitor {
 						let soc = bat.state_of_charge().value;
 						let percentage = (soc * 100.0).round() as i32;
 						let is_charging = matches!(bat.state(), battery::State::Charging);
-						Ok((percentage, is_charging))
+						Ok(BatteryInfo{percentage, is_charging})
 					}
 				}
 			}
--- src/battery_tray_icon.rs
+++ src/battery_tray_icon.rs
@@ -1,7 +1,7 @@
 use tray_icon::{TrayIcon, TrayIconBuilder, Icon};
 use tray_icon::menu::{Menu, MenuItem};
 
-use crate::battery_monitor::BatteryMonitor;
+use crate::battery_monitor::{BatteryMonitor, BatteryInfo};
 use crate::icon_builder::IconBuilder;
 use crate::debug_util::dmsg;
 
@@ -9,7 +9,7 @@ pub struct BatteryTrayIcon {
 	tray_icon: Option<TrayIcon>,
 	battery_monitor: BatteryMonitor,
 	icon_builder: IconBuilder,
-	current_percentage: i32,
+	cached_battery_info: Option<BatteryInfo>,
 }
 
 fn create_tray_icon(icon: Icon) -> Result<TrayIcon, String> {
@@ -35,23 +35,23 @@ impl BatteryTrayIcon {
 			tray_icon: None,
 			battery_monitor,
 			icon_builder,
-			current_percentage: -1, // Invalid value to force initial update
+			cached_battery_info: None, // None to force initial update
 		}
 	}
 
 	pub fn sync_tray_icon(&mut self) -> Result<(), String> {
 		// Get current battery info (percentage and charging status)
-		let (battery_percent, is_charging) = self.battery_monitor.get_battery_info()?;
+		let battery_info = self.battery_monitor.get_battery_info()?;
 
-		// Only update tray icon if percentage changed
-		if battery_percent == self.current_percentage {
+		// Only update tray icon if battery state changed
+		if Some(&battery_info) == self.cached_battery_info.as_ref() {
 			Ok(())
 		}
 		else {
-			self.current_percentage = battery_percent;
+			self.cached_battery_info = Some(battery_info.clone());
 
 			// Create new icon image
-			let Ok(icon_image) = self.icon_builder.create_percentage_icon(battery_percent, is_charging) else {
+			let Ok(icon_image) = self.icon_builder.create_percentage_icon(battery_info.percentage, battery_info.is_charging) else {
 				return Err(format!("Couldn't build icon"));
 			};
 			let icon = Icon::from_rgba(icon_image.clone().into_raw(), icon_image.width(), icon_image.height())
@@ -75,7 +75,10 @@ impl BatteryTrayIcon {
 						Err(format!("Failed to update tray icon: {:?}", e))
 					}
 					else {
-						dmsg!("Updated tray icon to {}%{}", battery_percent, if is_charging { " (charging)" } else { "" });
+						dmsg!("Updated tray icon to {}%{}",
+							battery_info.percentage,
+							if battery_info.is_charging { " (charging)" } else { "" }
+						);
 						Ok(())
 					}
 				}
